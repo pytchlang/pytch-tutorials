6701c4b016d842ad5e4c7a3f4737fee97fd4a1ab
Ben North <ben@redfrontdoor.org>
2020-06-19 11:22:21 +0100

    {#define-Bat-class} Bat: Add class

--- boing/code.py
+++ boing/code.py
@@ -14,6 +14,14 @@ class BoingBackground(Stage):
         self.switch_backdrop('boing')
 
 
+class Bat(Sprite):
+    Costumes = [('normal', 'images/bat00.png', 60, 60)]
+
+    def __init__(self):
+        Sprite.__init__(self)
+        self.switch_costume('normal')
+
+
 project = Project()
 project.register_stage_class(BoingBackground)
 project.go_live()
