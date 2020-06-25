b6decc4426ef19d3eb3eddfb16159d7cd7054c90
Glenn Strong <Glenn.Strong@scss.tcd.ie>
2020-06-25 16:32:05 +0100

    {#simple-player-sprite}

--- bunner/code.py
+++ bunner/code.py
@@ -11,3 +11,16 @@ class BunnyStage(Stage):
       Stage.__init__(self)
       self.switch_backdrop('world')
 
+class Bunny(Sprite):
+    Costumes = [
+        ('up', 'images/sit0.png', 30, 30),
+        ('right', 'images/sit1.png', 30, 30),
+        ('down', 'images/sit2.png', 30, 30),
+        ('left', 'images/sit3.png', 30, 30)
+        ]
+
+    def __init__(self):
+        Sprite.__init__(self)
+        self.switch_costume('up')
+        self.go_to_xy(0, -160)
+        self.show()
