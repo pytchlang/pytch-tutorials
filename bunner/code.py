d521b0bb37c4c9e6bc9b7f1f8ca59e38874dfc4e
Glenn Strong <Glenn.Strong@scss.tcd.ie>
2020-06-29 16:54:43 +0100

    {#check-for-squishing}
    React to a car reporting that it has squished the bunny

--- bunner/code.py
+++ bunner/code.py
@@ -99,3 +99,10 @@ class Car(pytch.Sprite):
         self.hide()
         self.delete_this_clone()
 
+    @pytch.when_I_start_as_a_clone
+    def check_for_collision(self):
+        while True:
+            while not self.touching(Bunny):
+                pass
+            print("Squish the bunny!")
+
