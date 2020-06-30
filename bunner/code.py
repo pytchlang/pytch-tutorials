f7e7b45248dc5465a937e331ac9e2b03a0066818
Glenn Strong <Glenn.Strong@scss.tcd.ie>
2020-06-30 17:13:17 +0100

    {#call-play-one-life}
    Play a single life

--- bunner/code.py
+++ bunner/code.py
@@ -46,10 +46,7 @@ class Bunny(Sprite):
     @when_green_flag_clicked
     def start_game(self):
         self.lives = 3
-        self.switch_costume('up')
-        self.go_to_xy(0, -160)
-        self.mode = PLAYING
-        self.show()
+        self.play_one_life()
 
 
     @when_key_pressed('ArrowUp')
