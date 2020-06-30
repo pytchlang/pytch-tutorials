723607e1fbac7ab1fd86d8b16ddc7759d42244c7
Glenn Strong <Glenn.Strong@scss.tcd.ie>
2020-06-30 16:02:28 +0100

    {#dont-move-unless-playing}
    Guard each movement routine so that it checks the bunny state

--- bunner/code.py
+++ bunner/code.py
@@ -33,27 +33,31 @@ class Bunny(pytch.Sprite):
 
     @pytch.when_key_pressed("ArrowUp")
     def move_up(self):
-        self.switch_costume("up")
-        if self.get_y() < 150:
-            self.change_y(40)
+        if self.mode == PLAYING:
+            self.switch_costume("up")
+            if self.get_y() < 150:
+                self.change_y(40)
 
     @pytch.when_key_pressed("ArrowRight")
     def move_right(self):
-        self.switch_costume("right")
-        if self.get_x() < 210:
-            self.change_x(25)
+        if self.mode == PLAYING:
+            self.switch_costume("right")
+            if self.get_x() < 210:
+                self.change_x(25)
 
     @pytch.when_key_pressed("ArrowDown")
     def move_down(self):
-        self.switch_costume("down")
-        if self.get_y() > -150:
-            self.change_y(-40)
+        if self.mode == PLAYING:
+            self.switch_costume("down")
+            if self.get_y() > -150:
+                self.change_y(-40)
 
     @pytch.when_key_pressed("ArrowLeft")
     def move_left(self):
-        self.switch_costume("left")
-        if self.get_x() > -210:
-            self.change_x(-25)
+        if self.mode == PLAYING:
+            self.switch_costume("left")
+            if self.get_x() > -210:
+                self.change_x(-25)
 
     @pytch.when_I_receive("squish bunny")
     def squish(self):
