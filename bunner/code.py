53dce6bae8b12c99fc4f3461547da38bb25e6623
Glenn Strong <Glenn.Strong@scss.tcd.ie>
2020-06-30 17:14:31 +0100

    {#play-another-life-after-death}
    Switch to calling play one life

--- bunner/code.py
+++ bunner/code.py
@@ -91,7 +91,7 @@ class Bunny(Sprite):
         self.mode = SQUISHED
         self.switch_costume( self._appearance + "_squished" )
         pytch.wait_seconds(0.5)
-        self.start_game()
+        self.play_one_life()
 
     
 
