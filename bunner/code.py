53357f99b9346c50b07d6a209955c311e033838c
Glenn Strong <Glenn.Strong@scss.tcd.ie>
2020-06-30 11:30:50 +0100

    {#special-hitbox-for-cars}
    The car and bunny sprites are too large for .touching() to reliably
    report collisions. Rather than editing the sprite graphics which would
    remove some nice shadows I've chosen to hand-roll collision detection.

--- bunner/code.py
+++ bunner/code.py
@@ -107,3 +107,8 @@ class Car(pytch.Sprite):
                 pass
             print("Squish the bunny!")
 
+    def hits(self, other):
+        return (
+            abs(self.get_y() - other.get_y()) <= 10
+            and abs(self.get_x() - other.get_x()) <= 40
+        )
