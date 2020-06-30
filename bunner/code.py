a76522cc5c3519c5b4b736543f1f2c9f1c541d41
Glenn Strong <Glenn.Strong@scss.tcd.ie>
2020-06-30 17:13:56 +0100

    {#define-play-one-life}
    Initial play one life

--- bunner/code.py
+++ bunner/code.py
@@ -38,6 +38,13 @@ class Bunny(pytch.Sprite):
         self.lives = 3
         self.play_one_life()
 
+    def play_one_life(self):
+        self.lives = self.lives - 1
+        self.switch_costume("up")
+        self.go_to_xy(0, -160)
+        self.mode = PLAYING
+        self.show()
+
     @pytch.when_key_pressed("ArrowUp")
     def move_up(self):
         if self.mode == PLAYING:
