53d763c912d79b10908ce299dbefa3b07f66164d
Glenn Strong <Glenn.Strong@scss.tcd.ie>
2020-07-01 23:18:22 +0100

    {#remove-logs-at-end-of-game}
    WHen the game ends the Log clones should all be deleted

--- bunner/code.py
+++ bunner/code.py
@@ -328,3 +328,8 @@ class Log(Sprite):
                     Bunny.the_original().change_x( self.speed )
         self.hide()
         self.delete_this_clone()
+
+    @when_I_receive('game over')
+    def vanish(self):
+        self.delete_this_clone() # Does nothing on the non-clone original
+
