4ac614d8132d9bef9ed284f4bb0515230b6782f0
Glenn Strong <Glenn.Strong@scss.tcd.ie>
2020-07-01 17:18:13 +0100

    {#broadcast-score-change-message}

--- bunner/code.py
+++ bunner/code.py
@@ -72,7 +72,7 @@ class Bunny(pytch.Sprite):
                 if self.current_row > self.highest_row_reached:
                     self.highest_row_reached = self.current_row
                     score = score + 1
-                    print( "Score is ", score )
+                    pytch.broadcast("score changed")
 
     @pytch.when_key_pressed("ArrowRight")
     def move_right(self):
