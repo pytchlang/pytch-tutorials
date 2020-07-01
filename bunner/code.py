a6c28d195a764037861d2b21bc816901eb8fb238
Glenn Strong <Glenn.Strong@scss.tcd.ie>
2020-07-01 23:15:31 +0100

    {#start-3-rows-of-logs}
    Three threads, each starting a slightly different log row

--- bunner/code.py
+++ bunner/code.py
@@ -260,6 +260,18 @@ class Log(pytch.Sprite):
 
     start_shown = False
 
+    @pytch.when_I_receive("start playing")
+    def start_row_1(self):
+        self.start_row("right", 30)
+
+    @pytch.when_I_receive("start playing")
+    def start_row_2(self):
+        self.start_row("left", 80)
+
+    @pytch.when_I_receive("start playing")
+    def start_row_3(self):
+        self.start_row("right", 130)
+
     def start_row(self, direction, y):
         self.speed = 3
         if direction == "right":
