5cf4b53d0a24dd412138152f221497a4e09a2e72
Glenn Strong <Glenn.Strong@scss.tcd.ie>
2020-07-01 16:52:42 +0100

    {#increment-current-row-counter}

--- bunner/code.py
+++ bunner/code.py
@@ -92,6 +92,7 @@ class Bunny(Sprite):
           self.switch_costume('up')
           if self.get_y() < 150:
               self.change_y(40)
+              self.current_row = self.current_row + 1
               score = score + 1
               print "Score is ", score
 
