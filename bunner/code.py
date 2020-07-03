76da68c84d3440aefe12ec44181ceb7b07b6cf19
Glenn Strong <Glenn.Strong@scss.tcd.ie>
2020-07-03 14:34:31 +0100

    {#detect-final-row}
    If we're on the final row then the bunny dances around while the score
    goes up

--- bunner/code.py
+++ bunner/code.py
@@ -133,6 +133,17 @@ class Bunny(pytch.Sprite):
             pytch.wait_seconds(0.5)
             self.play_one_life()
 
+    def check_victory(self):
+        global score
+        if self.y_position > 159:
+            self.mode = DANCING
+            for _ in range(4):
+                for facing in ["right", "down", "left", "up"]:
+                    self.switch_costume(facing)
+                    pytch.wait_seconds(0.125)
+                    score = score + 1
+                    pytch.broadcast("score changed")
+
 
 class Car(pytch.Sprite):
     Costumes = [
