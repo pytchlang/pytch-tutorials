c809a619074c1a141519f031fa26e1d007268bed
Glenn Strong <Glenn.Strong@scss.tcd.ie>
2020-07-03 13:21:49 +0100

    {#lose-a-life-to-drowning}
    No longer relevant

--- bunner/code.py
+++ bunner/code.py
@@ -86,6 +86,7 @@ class Bunny(pytch.Sprite):
                 for i in range(8):
                     self.switch_costume(f"splash-{i}")
                     pytch.wait_seconds(0.1)
+                pytch.wait_seconds(0.5)
 
     @pytch.when_key_pressed("ArrowUp")
     def move_up(self):
