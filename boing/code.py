7c05f8022dfa5cfad5f9aafe69ab34c797944759
Ben North <ben@redfrontdoor.org>
2020-08-11 11:43:29 +0100

    {#move-Player-with-W-and-S}

--- boing/code.py
+++ boing/code.py
@@ -16,3 +16,9 @@ class PlayerBat(pytch.Sprite):
     def play(self):
         self.go_to_xy(-215, 0)
         self.show()
+
+        while True:
+            if pytch.key_is_pressed("w"):
+                self.change_y(3)
+            if pytch.key_is_pressed("s"):
+                self.change_y(-3)
