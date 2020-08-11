0856dadd9138a34250ea9a1909d870458dfd777f
Ben North <ben@redfrontdoor.org>
2020-08-11 12:21:57 +0100

    {#make-Ball-y-speed-depend-on-Player-position}

--- boing/code.py
+++ boing/code.py
@@ -54,6 +54,7 @@ class Ball(pytch.Sprite):
                 player_y = PlayerBat.the_original().y_position
                 position_on_bat = self.y_position - player_y
                 if (position_on_bat >= -45) and (position_on_bat <= 45):
+                    self.y_speed = int(position_on_bat / 10)
                     self.change_x(-self.x_speed)
                     self.x_speed = -self.x_speed
 
