8c421ddc41dc54a630079cb8a63dfc7d1c4a8173
Ben North <ben@redfrontdoor.org>
2020-08-11 12:15:10 +0100

    {#check-hit-PlayerBat}

--- script-by-script-boing/code.py
+++ script-by-script-boing/code.py
@@ -45,5 +45,6 @@ class Ball(pytch.Sprite):
                 x_velocity = -x_velocity
 
             if self.x_position < -200:
-                self.change_x(-x_velocity)
-                x_velocity = -x_velocity
+                if self.touching(PlayerBat):
+                    self.change_x(-x_velocity)
+                    x_velocity = -x_velocity
