c131dd46328c64df3eb943d75ef07692c962555a
Ben North <ben@redfrontdoor.org>
2020-08-11 12:22:26 +0100

    {#make-Ball-bounce-vertically}

--- boing/code.py
+++ boing/code.py
@@ -59,3 +59,7 @@ class Ball(pytch.Sprite):
                     self.x_speed = -self.x_speed
 
             self.change_y(self.y_speed)
+
+            if self.y_position > 158 or self.y_position < -158:
+                self.change_y(-self.y_speed)
+                self.y_speed = -self.y_speed
