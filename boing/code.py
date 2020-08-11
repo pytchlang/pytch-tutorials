4e12842766477f57a35af2b83fe4d50774c7b742
Ben North <ben@redfrontdoor.org>
2020-08-11 12:02:12 +0100

    {#centre-Robot-on-green-flag}

--- boing/code.py
+++ boing/code.py
@@ -29,6 +29,11 @@ class RobotBat(pytch.Sprite):
         ('normal', 'images/bat10.png', 60, 60),
     ]
 
+    @pytch.when_green_flag_clicked
+    def play(self):
+        self.go_to_xy(215, 0)
+        self.show()
+
 
 class Ball(pytch.Sprite):
     Costumes = [
