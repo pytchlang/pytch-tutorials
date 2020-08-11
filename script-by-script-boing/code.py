b34acce7fcf57fabfe5fa10f8bba4ef71dbae4a2
Ben North <ben@redfrontdoor.org>
2020-08-11 12:10:22 +0100

    {#bounce-Ball-off-PlayerBat}

--- script-by-script-boing/code.py
+++ script-by-script-boing/code.py
@@ -43,3 +43,7 @@ class Ball(pytch.Sprite):
             if self.x_position > 200:
                 self.change_x(-x_velocity)
                 x_velocity = -x_velocity
+
+            if self.x_position < -200:
+                self.change_x(-x_velocity)
+                x_velocity = -x_velocity
