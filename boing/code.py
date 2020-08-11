9f99697f09ded8700b087c60049506f54a9a0116
Ben North <ben@redfrontdoor.org>
2020-08-11 11:43:11 +0100

    {#init-Player-on-green-flag}

--- boing/code.py
+++ boing/code.py
@@ -9,3 +9,8 @@ class PlayerBat(pytch.Sprite):
     Costumes = [
         ("normal", "bat00.png"),
     ]
+
+    @pytch.when_green_flag_clicked
+    def play(self):
+        self.go_to_xy(-215, 0)
+        self.show()
