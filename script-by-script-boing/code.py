85979987395b4d3bc4f89288d0a72919086aa966
Ben North <ben@redfrontdoor.org>
2020-08-11 12:55:19 +0100

    {#trigger-PlayerBat-flash-on-hit}

--- script-by-script-boing/code.py
+++ script-by-script-boing/code.py
@@ -65,6 +65,7 @@ class Ball(pytch.Sprite):
                     self.change_x(-x_velocity)
                     x_velocity = -x_velocity
                     y_velocity = random.randint(-4, 4)
+                    pytch.broadcast("player-hit")
                 else:
                     self.hide()
                     break
