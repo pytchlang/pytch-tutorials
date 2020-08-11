a67663f3e6a8f592711151b76b6536d66a6cb306
Ben North <ben@redfrontdoor.org>
2020-08-11 13:08:59 +0100

    {#random-Ball-y-velocity-RobotBat-bounce}

--- script-by-script-boing/code.py
+++ script-by-script-boing/code.py
@@ -44,6 +44,7 @@ class Ball(pytch.Sprite):
             if self.x_position > 200:
                 self.change_x(-x_velocity)
                 x_velocity = -x_velocity
+                y_velocity = random.randint(-4, 4)
 
             if self.x_position < -200:
                 if self.touching(PlayerBat):
