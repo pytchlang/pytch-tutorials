c5f47ff6fd78994ded64095e1bbe5b2d1d55dcb6
Glenn Strong <Glenn.Strong@scss.tcd.ie>
2020-09-17 10:18:38 +0100

    {#green-flag-show}
    Add green flag decorator to "start" function

--- chase-game/code.py
+++ chase-game/code.py
@@ -3,5 +3,6 @@ import pytch
 class Bird(pytch.Sprite):
     Costumes = [('bird', 'images/game-1582150_1280.png', 166, 108)]
 
+    @when_green_flag_clicked
     def start(self):
         self.show()
