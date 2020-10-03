baed65b88cc6b3653b1303e330525a886db7e23d
Ben North <ben@redfrontdoor.org>
2020-10-03 09:52:56 +0100

    {#Bunny-green-flag-go-to-start}

--- bunner/code.py
+++ bunner/code.py
@@ -7,3 +7,8 @@ class BunnyStage(pytch.Stage):
 
 class Bunny(pytch.Sprite):
     Costumes = [ "up.png", "right.png", "down.png", "left.png"]
+
+    @pytch.when_green_flag_clicked
+    def go_to_starting_position(self):
+        self.switch_costume("up")
+        self.go_to_xy(0, -160)
