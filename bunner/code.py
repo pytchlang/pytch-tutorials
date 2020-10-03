8a08498018fb1ad01b8f14757bab16f1c18d9364
Ben North <ben@redfrontdoor.org>
2020-10-03 09:52:40 +0100

    {#Bunny-with-costumes}

--- bunner/code.py
+++ bunner/code.py
@@ -3,3 +3,12 @@ import pytch
 
 class BunnyStage(pytch.Stage):
     Backdrops = [("world", "bunner-background.png")]
+
+
+class Bunny(pytch.Sprite):
+    Costumes = [
+        ("up", "sit0.png"),
+        ("right", "sit1.png"),
+        ("down", "sit2.png"),
+        ("left", "sit3.png"),
+    ]
