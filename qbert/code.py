6fa6431950e7b449fbc8c4bf4a3c26f014fe26ff
Ben North <ben@redfrontdoor.org>
2020-10-15 14:19:09 +0100

    {#record-row-block-for-clone}

--- qbert/code.py
+++ qbert/code.py
@@ -15,6 +15,8 @@ class Block(pytch.Sprite):
             for b in range(7 - r):
                 self.go_to_xy(-150 + (b * 56) + (r * 28),
                               -145 + (r * 42))
+                self.pyramid_r = r
+                self.pyramid_b = b
                 pytch.create_clone_of(self)
         pytch.broadcast("set-up-qbert")
 
