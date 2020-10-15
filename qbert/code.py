9dbf2357e3a3295e7ae655a71cd6aa4bca682681
Ben North <ben@redfrontdoor.org>
2020-10-15 14:15:41 +0100

    {#set-Qbert-full-size-when-starting}

--- qbert/code.py
+++ qbert/code.py
@@ -34,6 +34,7 @@ class Qbert(pytch.Sprite):
     @pytch.when_I_receive("set-up-qbert")
     def go_to_starting_position(self):
         self.go_to_xy(-150 + 3 * 56, -145 + (6 * 42) + 28)
+        self.set_size(1.0)
         self.switch_costume("qbert1")
         self.move_to_front_layer()
         self.show()
