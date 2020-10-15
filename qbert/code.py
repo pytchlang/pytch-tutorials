16e3b8481a04e2fbc8de1b3be6327d53cb701e2b
Ben North <ben@redfrontdoor.org>
2020-10-15 12:49:22 +0100

    {#create-block-pyramid}

--- qbert/code.py
+++ qbert/code.py
@@ -8,3 +8,11 @@ class Background(pytch.Stage):
 class Block(pytch.Sprite):
     Costumes = ["block0.png", "block1.png"]
     start_shown = False
+
+    @pytch.when_green_flag_clicked
+    def create_pyramid(self):
+        for r in range(7):
+            for b in range(7 - r):
+                self.go_to_xy(-150 + (b * 56) + (r * 28),
+                              -145 + (r * 42))
+                pytch.create_clone_of(self)
