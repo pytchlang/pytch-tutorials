533738038d600d985e1a4d22ed02a994388cb0cc
Glenn Strong <Glenn.Strong@scss.tcd.ie>
2020-11-12 12:07:47 +0000

    Revert "{#show-bird}"
    This reverts commit ae01951e4e9811e50eae58749f85c23d4724e39f.

--- chase/code.py
+++ chase/code.py
@@ -7,9 +7,3 @@ class Bird(pytch.Sprite):
     Costumes = ['Bird.png']
 
     start_shown = False
-
-    @pytch.when_green_flag_clicked
-    def start(self):
-        self.go_to_xy(0,0)
-        self.set_size(0.3)
-        self.show()
