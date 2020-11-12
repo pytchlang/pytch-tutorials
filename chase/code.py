01de412975e5b34230f6519e2a925a9386aabc20
Glenn Strong <Glenn.Strong@scss.tcd.ie>
2020-11-12 12:08:22 +0000

    {#show-bird}
    Show bird on green flag

--- chase/code.py
+++ chase/code.py
@@ -9,3 +9,8 @@ class Bird(pytch.Sprite):
     Costumes = ["bird.png"]
 
     start_shown = False
+
+    @pytch.when_green_flag_clicked
+    def start(self):
+        self.go_to_xy(0, 0)
+        self.show()
