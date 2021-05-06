c723e584a155c5dcc8bc1285376fcb2acac5ab02
Glenn Strong <Glenn.Strong@scss.tcd.ie>
2021-05-06 12:48:12 +0100

    {#non-yielding-hit-check-loop}
    Add the non-yielding-loop decorator to the custom hit-checked
    
    When checking for a collision we want the loop to run to completion
    before any of the sprites in question are moved.

--- bunner/code.py
+++ bunner/code.py
@@ -63,6 +63,7 @@ class Bunny(pytch.Sprite):
             self.hide()
             self.mode = WAITING
 
+    @pytch.non_yielding_loops
     def touching_any_log(self):
         for log in Log.all_clones():
             if log.hits(self):
