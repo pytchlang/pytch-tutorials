a4a071967ca7935d21b51c1c6c61b78943190bd0
Ben North <ben@redfrontdoor.org>
2021-05-17 12:12:28 +0100

    {#broadcast-when-apple-caught}

--- script-by-script-catch-apple/code.py
+++ script-by-script-catch-apple/code.py
@@ -50,6 +50,7 @@ class Apple(pytch.Sprite):
             self.change_y(-3)
             if self.touching(Bowl):
                 self.hide()
+                self.broadcast("award-point")
 
 
 class ScoreKeeper(pytch.Sprite):
