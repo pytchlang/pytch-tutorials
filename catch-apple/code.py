d3271dfab11caf782560dd6955465347de2f8804
Ben North <ben@redfrontdoor.org>
2021-05-17 12:15:30 +0100

    {#launch-apple-on-message-not-green-flag}

--- catch-apple/code.py
+++ catch-apple/code.py
@@ -21,7 +21,7 @@ class Bowl(pytch.Sprite):
 class Apple(pytch.Sprite):
     Costumes = ["apple.png"]
 
-    @pytch.when_green_flag_clicked
+    @pytch.when_I_receive("launch-apple")
     def move_down_screen(self):
         drop_x = random.randint(-145, 190)
         self.go_to_xy(drop_x, 200)
