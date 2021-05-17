307c69b7684b02121687bd327a7794f8964bce0f
Ben North <ben@redfrontdoor.org>
2021-05-17 12:08:25 +0100

    {#add-empty-Apple-class}

--- script-by-script-catch-apple/code.py
+++ script-by-script-catch-apple/code.py
@@ -17,3 +17,7 @@ class Bowl(pytch.Sprite):
             if pytch.key_pressed("d"):
                 if self.x_position < 190:
                     self.change_x(2)
+
+
+class Apple(pytch.Sprite):
+    Costumes = []
