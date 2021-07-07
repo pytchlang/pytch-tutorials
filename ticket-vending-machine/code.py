4a1b1dc111e50c3566469b7ab594bfa6c8107372
Ben North <ben@redfrontdoor.org>
2021-07-07 22:38:26 +0100

    {#show-child-ticket}

--- ticket-vending-machine/code.py
+++ ticket-vending-machine/code.py
@@ -3,3 +3,8 @@ import pytch
 
 class Ticket(pytch.Sprite):
     Costumes = ["child.png", "adult.png", "family.png"]
+
+    @pytch.when_green_flag_clicked
+    def show_tickets(self):
+        self.switch_costume("child")
+        self.go_to_xy(0, 120)
