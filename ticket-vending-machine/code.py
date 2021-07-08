c244a520c102c1c910958649af3fa3e1ba9c66d3
Ben North <ben@redfrontdoor.org>
2021-07-08 17:30:46 +0100

    {#show-adult-ticket}

--- ticket-vending-machine/code.py
+++ ticket-vending-machine/code.py
@@ -8,3 +8,6 @@ class Ticket(pytch.Sprite):
     def show_tickets(self):
         self.switch_costume("child")
         self.go_to_xy(0, 120)
+        pytch.create_clone_of(self)
+        self.switch_costume("adult")
+        self.go_to_xy(0, 0)
