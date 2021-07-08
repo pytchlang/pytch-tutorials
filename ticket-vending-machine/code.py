e9e19e4243d8231471241a58603e867eb03d109c
Ben North <ben@redfrontdoor.org>
2021-07-08 20:50:00 +0100

    {#store-ticket-costs}

--- ticket-vending-machine/code.py
+++ ticket-vending-machine/code.py
@@ -7,10 +7,13 @@ class Ticket(pytch.Sprite):
     @pytch.when_green_flag_clicked
     def show_tickets(self):
         self.switch_costume("child")
+        self.cost = 6
         self.go_to_xy(0, 120)
         pytch.create_clone_of(self)
         self.switch_costume("adult")
+        self.cost = 10
         self.go_to_xy(0, 0)
         pytch.create_clone_of(self)
         self.switch_costume("family")
+        self.cost = 20
         self.go_to_xy(0, -120)
