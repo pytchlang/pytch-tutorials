f49d024dc2bf4c461eabe0e0a8c416312979dd17
Ben North <ben@redfrontdoor.org>
2022-03-31 16:15:34 +0100

    {#start-appropriate-sound-when-hit}

--- blue-invader/code.py
+++ blue-invader/code.py
@@ -28,6 +28,13 @@ class Alien(pytch.Sprite):
             glide_time = random.uniform(3.0, 5.0)
             self.glide_to_xy(self.x_position, -180, glide_time)
 
+    @pytch.when_this_sprite_clicked
+    def handle_hit(self):
+        if self.costume_number == 0:
+            self.start_sound("explosion")
+        else:
+            self.start_sound("scream")
+
 
 class Galaxy(pytch.Stage):
     Backdrops = ["starry-sky.jpg"]
