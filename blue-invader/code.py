023dec2634f84a42e408d834eab4c0453acb08f7
Ben North <ben@redfrontdoor.org>
2022-03-31 16:03:08 +0100

    {#loop-drift-down-screen}

--- blue-invader/code.py
+++ blue-invader/code.py
@@ -4,6 +4,12 @@ import pytch
 class Alien(pytch.Sprite):
     Costumes = ["enemy-alien.png", "friendly-alien.png"]
 
+    @pytch.when_green_flag_clicked
+    def drift_down_screen(self):
+        while True:
+            self.set_y(180)
+            self.glide_to_xy(self.x_position, -180, 3.0)
+
 
 class Galaxy(pytch.Stage):
     Backdrops = ["starry-sky.jpg"]
