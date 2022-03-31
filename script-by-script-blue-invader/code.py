7086b5d9e4f76d432dfa1f912a9cf4ba095eafd4
Ben North <ben@redfrontdoor.org>
2022-03-31 16:17:57 +0100

    {#show-when-starting-descent}

--- script-by-script-blue-invader/code.py
+++ script-by-script-blue-invader/code.py
@@ -54,6 +54,7 @@ class Alien(pytch.Sprite):
             costume_index = random.choice([0, 2])
             self.switch_costume(costume_index)
             self.set_y(180)
+            self.show()
             glide_time = random.uniform(3.0, 5.0)
             self.glide_to_xy(self.x_position, -180, glide_time)
 
