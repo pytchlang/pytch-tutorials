588abbd69d9a347c910a6b32861ed2d8101f2347
Ben North <ben@redfrontdoor.org>
2022-03-31 16:47:02 +0100

    {#extract-lose-life-method}

--- blue-invader/code.py
+++ blue-invader/code.py
@@ -38,6 +38,9 @@ class Alien(pytch.Sprite):
             self.start_sound("scream")
         self.hide()
 
+    def lose_life(self):
+        self.start_sound("scream")
+
 
 class Galaxy(pytch.Stage):
     Backdrops = ["starry-sky.jpg"]
