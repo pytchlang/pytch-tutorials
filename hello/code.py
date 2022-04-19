1bdc03f5100c4509bbbae6b4cb12aa0ebcce7cfc
justinemoulin <justinemoulin5@icloud.com>
2022-04-19 10:54:43 +0100

    {#level-difficulty}

--- hello/code.py
+++ hello/code.py
@@ -6,3 +6,6 @@ class Snake(pytch.Sprite):
     @pytch.when_this_sprite_clicked
     def speak(self):
         self.say_for_seconds("Hello there!", 2.0)
+
+# difficulty level => 0:"introduction", 1:"easy", 3:"medium", 4:"difficult"
+level = 0
