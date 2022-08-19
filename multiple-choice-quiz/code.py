6125e2db083df77f16b054d7aeac437a95935e82
Ben North <ben@redfrontdoor.org>
2022-08-19 17:46:21 +0100

    {#make-Narrator-announce-quiz}
    Will soon shorten the "3" when we add some more say()s.  Explain
    "think about how to speed up development, don't waste your time".

--- multiple-choice-quiz/code.py
+++ multiple-choice-quiz/code.py
@@ -3,3 +3,7 @@ import pytch
 
 class Narrator(pytch.Sprite):
     Costumes = ["button-question.png"]
+
+    @pytch.when_green_flag_clicked
+    def play_quiz(self):
+        self.say_for_seconds("Let's begin!", 3)
