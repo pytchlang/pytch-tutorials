9fe4e09dad1a30fe155aadcdeb266ebbff581ebe
Ben North <ben@redfrontdoor.org>
2022-08-19 18:18:15 +0100

    {#indent-what-is-now-while-loop-body}

--- multiple-choice-quiz/code.py
+++ multiple-choice-quiz/code.py
@@ -32,17 +32,17 @@ class Narrator(pytch.Sprite):
 
         while question_index < n_questions:
 
-        question_info = all_questions_info[question_index]
+            question_info = all_questions_info[question_index]
 
-        question = question_info[0]
-        ans_A = "A: " + question_info[1]
-        ans_B = "B: " + question_info[2]
-        ans_C = "C: " + question_info[3]
+            question = question_info[0]
+            ans_A = "A: " + question_info[1]
+            ans_B = "B: " + question_info[2]
+            ans_C = "C: " + question_info[3]
 
-        text = question + "\n" + ans_A + "\n" + ans_B + "\n" + ans_C
-        self.say(text)
+            text = question + "\n" + ans_A + "\n" + ans_B + "\n" + ans_C
+            self.say(text)
 
-        pytch.wait_seconds(2)
+            pytch.wait_seconds(2)
 
         question_index = 1
 
