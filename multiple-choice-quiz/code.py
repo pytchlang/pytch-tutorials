be3d92b0086e55b1f0465680123c4d5cb7ad6e69
Ben North <ben@redfrontdoor.org>
2022-08-19 18:39:14 +0100

    {#label-answer-C-text}

--- multiple-choice-quiz/code.py
+++ multiple-choice-quiz/code.py
@@ -24,7 +24,7 @@ class Narrator(pytch.Sprite):
         question = question_info[0]
         ans_A = "A: " + question_info[1]
         ans_B = "B: " + question_info[2]
-        ans_C = question_info[3]
+        ans_C = "C: " + question_info[3]
 
         text = question + "\n" + ans_A + "\n" + ans_B + "\n" + ans_C
         self.say(text)
