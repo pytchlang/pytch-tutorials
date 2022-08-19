4a2ae2dc52e649ddaaa51719eadcb992ae850d7a
Ben North <ben@redfrontdoor.org>
2022-08-19 18:38:48 +0100

    {#label-answer-A-text}

--- multiple-choice-quiz/code.py
+++ multiple-choice-quiz/code.py
@@ -22,7 +22,7 @@ class Narrator(pytch.Sprite):
         question_info = all_questions_info[0]
 
         question = question_info[0]
-        ans_A = question_info[1]
+        ans_A = "A: " + question_info[1]
         ans_B = question_info[2]
         ans_C = question_info[3]
 
