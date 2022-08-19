b341b9334a81e0af201be33268801e941d1a5fcf
Ben North <ben@redfrontdoor.org>
2022-08-19 18:12:22 +0100

    {#copy-paste-to-ask-question-0-again}

--- multiple-choice-quiz/code.py
+++ multiple-choice-quiz/code.py
@@ -37,3 +37,13 @@ class Narrator(pytch.Sprite):
         self.say(text)
 
         pytch.wait_seconds(2)
+
+        question_info = all_questions_info[0]
+
+        question = question_info[0]
+        ans_A = "A: " + question_info[1]
+        ans_B = "B: " + question_info[2]
+        ans_C = "C: " + question_info[3]
+
+        text = question + "\n" + ans_A + "\n" + ans_B + "\n" + ans_C
+        self.say(text)
