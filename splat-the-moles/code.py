403f0d9e5df5ec4716674f408cf9853854c67d2b
Ben North <ben@redfrontdoor.org>
2022-10-14 11:43:08 +0100

    {#Mole-loop-random-holes}

--- splat-the-moles/code.py
+++ splat-the-moles/code.py
@@ -17,3 +17,7 @@ class Mole(pytch.Sprite):
     @pytch.when_green_flag_clicked
     def pop_up_and_down(self):
         self.go_to_xy(0, -70)
+
+        while True:
+            self.switch_costume(random.randint(1, 3))
+            pytch.wait_seconds(random.uniform(0.5, 1.0))
