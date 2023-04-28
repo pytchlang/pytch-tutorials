218f55862e064744326c164c3bc6018b8ac11bc0
Ben North <ben@redfrontdoor.org>
2023-04-28 13:04:35 +0100

    {#init-LED2-position}

--- trimon/code.py
+++ trimon/code.py
@@ -24,6 +24,10 @@ class LED2(pytch.Sprite):
     Costumes = ["light-off.png", "light-on.png"]
     Sounds = ["note-2.mp3"]
 
+    @pytch.when_green_flag_clicked
+    def move_to_position(self):
+        self.go_to_xy(0, 80)
+
 
 class LED3(pytch.Sprite):
     Costumes = ["light-off.png", "light-on.png"]
