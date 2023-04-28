0711444d481f467436944f80625cf81a449b7bda
Ben North <ben@redfrontdoor.org>
2023-04-28 14:16:19 +0100

    {#guard-Button3-press}

--- trimon/code.py
+++ trimon/code.py
@@ -103,4 +103,9 @@ class Button3(pytch.Sprite):
 
     @pytch.when_this_sprite_clicked
     def press_button(self):
+        global light_flashing
+        if light_flashing:
+            return
+        light_flashing = True
         pytch.broadcast_and_wait("flash-3")
+        light_flashing = False
