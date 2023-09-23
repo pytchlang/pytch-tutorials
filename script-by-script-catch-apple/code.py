bb1c2511905e772048609aafebf8d934557a9d03
Ben North <ben.north@tcd.ie>
2023-09-23 15:26:10 +0100

    {#add-empty-award-point-handler}

--- script-by-script-catch-apple/code.py
+++ script-by-script-catch-apple/code.py
@@ -66,3 +66,7 @@ class ScoreKeeper(pytch.Sprite):
         self.go_to_xy(-260, -105)
         self.score = 0
         self.say(self.score)
+
+    @pytch.when_I_receive("award-point")
+    def award_point(self):
+        pass
