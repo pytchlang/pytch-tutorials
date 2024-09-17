c03becdbfe7104b005c60044e6464c78795f3358
Ben North <ben.north@tcd.ie>
2024-09-17 15:12:46 +0100

    {#make-rest-of-clones}

--- script-by-script-blue-invader/code.py
+++ script-by-script-blue-invader/code.py
@@ -33,6 +33,14 @@ class Alien(pytch.Sprite):
         self.go_to_xy(-150, 180)
         pytch.create_clone_of(self)
         self.go_to_xy(-90, 180)
+        pytch.create_clone_of(self)
+        self.go_to_xy(-30, 180)
+        pytch.create_clone_of(self)
+        self.go_to_xy(30, 180)
+        pytch.create_clone_of(self)
+        self.go_to_xy(90, 180)
+        pytch.create_clone_of(self)
+        self.go_to_xy(150, 180)
 
     @pytch.when_green_flag_clicked
     def drift_down_screen(self):
