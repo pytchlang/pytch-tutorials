5d16f9006120d6f8e4864e31301c61f73b40547d
Ben North <ben.north@tcd.ie>
2024-09-17 15:12:35 +0100

    {#make-first-clone}

--- script-by-script-blue-invader/code.py
+++ script-by-script-blue-invader/code.py
@@ -31,6 +31,8 @@ class Alien(pytch.Sprite):
     @pytch.when_green_flag_clicked
     def make_clones(self):
         self.go_to_xy(-150, 180)
+        pytch.create_clone_of(self)
+        self.go_to_xy(-90, 180)
 
     @pytch.when_green_flag_clicked
     def drift_down_screen(self):
