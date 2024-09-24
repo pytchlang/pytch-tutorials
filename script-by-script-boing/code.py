95f45a42385108929b25785c6a5b1bfa34f8ca04
Ben North <ben.north@tcd.ie>
2024-09-24 10:53:51 +0100

    {#add-Ball-sprite}

--- script-by-script-boing/code.py
+++ script-by-script-boing/code.py
@@ -19,3 +19,7 @@ class PlayerBat(pytch.Sprite):
                 self.change_y(3)
             if pytch.key_pressed("s") and self.y_position > -112:
                 self.change_y(-3)
+
+
+class Ball(pytch.Sprite):
+    Costumes = []
