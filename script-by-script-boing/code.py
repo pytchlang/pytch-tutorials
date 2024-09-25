c1a5e73d5e44dc54a9a891c30dfcaedf2f132492
Ben North <ben.north@tcd.ie>
2024-09-25 11:57:04 +0100

    {#clamp-RobotBat-y-low}

--- script-by-script-boing/code.py
+++ script-by-script-boing/code.py
@@ -32,6 +32,8 @@ class RobotBat(pytch.Sprite):
             target_y = Ball.the_original().y_position
             if target_y > 112:
                 target_y = 112
+            if target_y < -112:
+                target_y = -112
             self.set_y(target_y)
 
 
