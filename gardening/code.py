d7cc299eef6ea7e6568d2e8dd9bf22eabaf7865d
mnang <m.nangole@gmx.at>
2026-06-16 11:45:55 +0100

    {#decrement-apples-variable}

--- gardening/code.py
+++ gardening/code.py
@@ -89,7 +89,7 @@ class Soil(pytch.Sprite):
     @pytch.when_key_pressed("p")
     def plant_tree(self):
         if Stage.apples > 0 and self.touching(Hover):
-            pass
+            Stage.apples -= 1
 
 
 class Water(pytch.Sprite):
