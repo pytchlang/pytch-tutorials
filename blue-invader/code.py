a3ee1cd73be43b5f89e8c1fb3af05baaa97e1cf3
Ben North <ben.north@tcd.ie>
2026-07-02 19:19:11 +0100

    {#set-initial-alien-size}

--- blue-invader/code.py
+++ blue-invader/code.py
@@ -4,6 +4,10 @@ import pytch
 class Alien(pytch.Sprite):
     Costumes = ["Invader_Navy_1.png", "Invader_Green_1.png"]
 
+    @pytch.when_green_flag_clicked
+    def make_sensible_size(self):
+        self.set_size(0.15)
+
 
 class Galaxy(pytch.Stage):
     Backdrops = ["starry-sky.jpg"]
